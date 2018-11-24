1b0322b543e6c99dfff2769725494306bfb87cd1
uniq <uniq@o2.pl>
2018-11-24 20:24:25 +0100

    debug is off

--- debug.h
+++ debug.h
@@ -1,4 +1,10 @@
 #include "memory_free.h"
-
+/*
+#define SerialMsg(a) { Serial.println(a); Serial.flush(); }
 #define SerialLog(a) { Serial.print(#a); Serial.print(": "); Serial.println(a); Serial.flush(); }
 #define SerialFreeMemLog() { Serial.print("Free mem: "); Serial.println(freeMemory()); Serial.flush(); }
+/*/
+#define SerialMsg(a)
+#define SerialLog(a)
+#define SerialFreeMemLog()
+//*/
